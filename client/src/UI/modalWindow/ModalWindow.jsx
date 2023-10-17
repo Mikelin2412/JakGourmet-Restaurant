ff70061b9f2b2244ae8f633f7bf2051dabf90b59
Mikhail Belyachevskiy <mishabelyachevsky@gmail.com>
2023-10-17 23:59:40 +0300

    feat: add fields into reservation popup

--- client/src/UI/modalWindow/ModalWindow.jsx
+++ client/src/UI/modalWindow/ModalWindow.jsx
@@ -3,8 +3,8 @@ import './modalWindow.css'
 
 const ModalWindow = ({active, setActive, children}) => {
   return (
-    <div className={(active) ? 'modal-window active' : 'modal-window'} onClick={() => {setActive(false)}}>
-      <div className={(active) ? 'modal-window-content active' : 'modal-window-content'} onClick={e => e.stopPropagation()}>
+    <div className={(active) ? 'modal-window active-modal' : 'modal-window'} onClick={() => {setActive(false)}}>
+      <div className={(active) ? 'modal-window-content active-modal' : 'modal-window-content'} onClick={e => e.stopPropagation()}>
         {children}
       </div>
     </div>
--- client/src/pages/Bucket.js
+++ client/src/pages/Bucket.js
@@ -87,7 +87,23 @@ const Bucket = () => {
         <ModalWindow
           active={modalActive}
           setActive={setModalActive}>
-            <h1>Бронирование</h1>
+            <form method="post">
+              <h1 className='modal__reservation__title'>Бронирование</h1>
+              <div className='modal__reservation__fields-block'>
+                <label id='name'>Имя: </label>
+                <input id='name' type='text'></input>
+                <label id='surname'>Фамилия: </label>
+                <input id='surname' type='text'></input>
+                <label id='date'>Дата: </label>
+                <input id='date' type='date'></input>
+                <label id='time'>Время: </label>
+                <input id='time' type='time'></input>
+                <label id='tel'>Телефон: </label>
+                <input id='tel' type='tel'></input>
+                <label id='guest-count'>Количество гостей: </label>
+                <input id='guest-count' type='number'></input>
+              </div>
+            </form>
         </ModalWindow>
       </div>
     </DishContext.Provider>
