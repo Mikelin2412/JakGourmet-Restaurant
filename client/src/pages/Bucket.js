f4911b4c049351c0d1cbdf45e32de42cf68c41ee
Mikhail Belyachevskiy <mishabelyachevsky@gmail.com>
2023-10-18 10:33:39 +0300

    refactor: transfer reservPopup in separate comp

--- client/src/pages/Bucket.js
+++ client/src/pages/Bucket.js
@@ -5,6 +5,7 @@ import '../styles/Bucket.css'
 import Footer from '../components/Footer'
 import BucketButton from '../UI/bucketButton/BucketButton'
 import ModalWindow from '../UI/modalWindow/ModalWindow'
+import ReservationPopup from '../components/ReservationPopup'
 
 export const DishContext = createContext({});
 
@@ -84,27 +85,9 @@ const Bucket = () => {
           </div>
         </div>
         <Footer />
-        <ModalWindow
+        <ReservationPopup
           active={modalActive}
-          setActive={setModalActive}>
-            <form method="post">
-              <h1 className='modal__reservation__title'>Бронирование</h1>
-              <div className='modal__reservation__fields-block'>
-                <label id='name'>Имя: </label>
-                <input id='name' type='text'></input>
-                <label id='surname'>Фамилия: </label>
-                <input id='surname' type='text'></input>
-                <label id='date'>Дата: </label>
-                <input id='date' type='date'></input>
-                <label id='time'>Время: </label>
-                <input id='time' type='time'></input>
-                <label id='tel'>Телефон: </label>
-                <input id='tel' type='tel'></input>
-                <label id='guest-count'>Количество гостей: </label>
-                <input id='guest-count' type='number'></input>
-              </div>
-            </form>
-        </ModalWindow>
+          setActive={setModalActive}/>
       </div>
     </DishContext.Provider>
   )
