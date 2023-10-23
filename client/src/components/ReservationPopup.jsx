16d1695dea4117d57bdd2de5d38a12ba3043a651
Mikhail Belyachevskiy <mr_davidov2412@mail.ru>
2023-10-23 22:39:09 +0300

    refactor: change reservationForm fields

--- client/src/components/ReservationPopup.jsx
+++ client/src/components/ReservationPopup.jsx
@@ -9,16 +9,13 @@ const ReservationPopup = ({active, setActive}) => {
             <form method="post">
                 <h1 className='modal__reservation__title'>Бронирование</h1>
                 <div className='modal__reservation__fields-block'>
-                    <label id='name'>Имя: </label>
-                    <input id='name' type='text'></input>
-                    <label id='surname'>Фамилия: </label>
-                    <input id='surname' type='text'></input>
                     <label id='date'>Дата: </label>
                     <input id='date' type='date'></input>
                     <label id='time'>Время: </label>
                     <input id='time' type='time'></input>
                     <label id='tel'>Телефон: </label>
                     <input id='tel' type='tel'></input>
+                    {/*количество гостей переделать на выбор свободного столик с помощью option*/}
                     <label id='guest-count'>Количество гостей: </label>
                     <input id='guest-count' type='number'></input>
                 </div>
