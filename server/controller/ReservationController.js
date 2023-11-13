039ed769a95fe80663d1c28140c5b001fac34423
Mikhail Belyachevskiy <mr_davidov2412@mail.ru>
2023-11-13 10:43:02 +0300

    feat: init methods into ReservationController

--- server/controller/ReservationController.js
+++ server/controller/ReservationController.js
@@ -4,6 +4,14 @@ class ReservationController {
     async addReservation(req, res) {
         return res.json('WORKING');
     }
+
+    async getAllReservations(req, res) {
+
+    }
+
+    async getDefiniteReservation(req, res) {
+        
+    }
 }
 
 module.exports = new ReservationController();
--- server/routes/ReservationRouter.js
+++ server/routes/ReservationRouter.js
@@ -3,5 +3,7 @@ const ReservationController = require('../controller/ReservationController');
 const router = new Router();
 
 router.post('/add', ReservationController.addReservation);
+router.get('/getAll', ReservationController.getAllReservations);
+router.get('/get/:id', ReservationController.getDefiniteReservation);
 
 module.exports = router;
